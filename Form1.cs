b63b1e8d305938c54a1ddc9bbf2be1b2f70df889
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-05-10 12:27:40 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EditorTeksta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold);
+            /*/if (btnItalic != null)
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Italic);
+            }
+            else if (btnUnder != null)
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Underline);
+            }
+            else
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Underline | FontStyle.Italic);
+            
+            }
+            */
+        }
+  
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            {
+                if (!Directory.Exists(@"c:\Editor"))
+                {
+                    Directory.CreateDirectory(@"c:\Editor");
+                }
+
+                else if (Directory.Exists(@"c:\Editor"))
+                {
+
+                    string path = @"c:\Editor\editor.rtf";
+                    if (File.Exists(path))
+                    {
+                        StreamWriter sw = new StreamWriter(path);
+                        sw.WriteLine(richTextBox1.Rtf);
+                        sw.Close();
+                    }
+                    else
+                    {
+                        File.Create(path);
+                    }
+                }
+
+
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string path = @"c:\Editor\editor.rtf";
+            StreamReader stream = new StreamReader(path);
+            string file = stream.ReadToEnd();
+            richTextBox1.Rtf = file.ToString();
+            stream.Close();
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Italic);
+        }
+
+        private void btnUnder_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Underline);
+        }
+
+        private void btnPlain_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Regular);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Regular);
+        }
+    }
+}
